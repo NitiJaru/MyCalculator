2e2263b429069232c6e4d01c80038d7f0385d26d
Perawatt <pekung4433@perfenterprise.com>
2018-09-15 16:59:51 +0700

    get API calculate

--- src/myIonic/src/pages/home/home.ts
+++ src/myIonic/src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  answer : string;
+  input: string;
+  constructor(public navCtrl: NavController,private httpClient: HttpClient) {
+    this.input = "1";
   }
-
+  calculate() 
+  {  
+    this.httpClient.get("http://node16.codenvy.io:47446/api/Calculator/GetSecondAnswer/"+this.input)
+      .subscribe((data: any) => {
+        this.answer = data.secondAnswer;
+        alert(data.secondAnswer);
+      }, error => {
+        // If error
+      });
+  } 
 }
